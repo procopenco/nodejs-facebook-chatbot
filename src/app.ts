a24a03af08ce4b992faf6b9d33e2f9d28718f0b4
Grajdean Serghei <grajdeanserghei@gmail.com>
2018-07-31 00:33:18 +0300

    application splited in server and app

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ import express from 'express';
 // Imports dependencies and set up http server
 const app = express();
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.get('/test', (_, res)=>{
-  res.send('Hello world');
+  res.send('Hello worldSS!');
 });
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import app from "./app";
+import http from "http";
+
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+// app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+// Sets server port and logs message on success
+let port  = process.env.PORT || 1338;
+// app.set('port', port);
+
+console.log('application port will be ' + port);
+
+var server = http.createServer(app);
+
+console.log('Start listening...');
+server.listen(port);
+
+// app.listen(port, ()=>{
+//   console.log(
+//     "  App is running at http://localhost:%d in %s mode",
+//     app.get("port"),
+//     app.get("env")
+//   );
+//   console.log("  Press CTRL-C to stop\n");  
+// });
+
+// app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+
+// const server = app.listen(app.get("port"), () => {
+//   console.log(
+//     "  App is running at http://localhost:%d in %s mode",
+//     app.get("port"),
+//     app.get("env")
+//   );
+//   console.log("  Press CTRL-C to stop\n");
+// });
+
+export default server;
